87279c81c7969912ced58f3f62878a9c82627a10
MatthewOfomi <ofomimatthew7@gmail.com>
2024-10-23 23:30:21 +0100

    menu function done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,47 @@
-// show menu
-const showMenu = (toggleId,navId)=>{
-  const toggle = document.getElementById(toggleId)
-  const nav = document.getElementById(navId)
-  toggle.addEventListener('click',()=>{
-    // show-menu class to nav menu
-    nav.classList.toggle('show-menu')
-
-    // show-icon class to show and hide menu icon
-    toggle.classList.toggle('show-icon')
-  })
-}
-
-showMenu('nav-toggle','nav-menu')
-
-// ==== dropdown menu======
-
-const dropdownItems = document.querySelectorAll('.dropdown__item')
-dropdownItems.forEach((item)=>{
-  const dropdownButton = item.querySelector('.dropdown__button')
-  dropdownButton.addEventListener('click',()=>{
-    const showDropdown = document.querySelector('.show-dropdown')
-    toggleItem(item)
-    if(showDropdown && showDropdown!== item){
-      toggleItem(showDropdown)
-    }
-  })
-})
 
 
-// === display dropdown===
-const toggleItem = (item)=>{
-const dropdownContainer = item.querySelector('.dropdown__container')
-if(item.classList.contains('show-dropdown')){
-  dropdownContainer.removeAttribute('style')
-  item.classList.remove('show-dropdown')
-}else{
-  dropdownContainer.style.height = dropdownContainer.scrollHeight + 'px'
-  item.classList.add('show-dropdown')
-}
+// ======= NAVIGATION ======
 
-}
 
-// ------- delete dropdown styles
-const mediaQuery = matchMedia('(min-width:1118px)')
-const dropdownContainer  = document.querySelectorAll('.dropdown__container')
 
-// function to remove dropdown styles in mobile view
-const removeStyle = ()=>{
-  if(mediaQuery.matches){
-    dropdownContainer.forEach((e)=>{
-      e.removeAttribute('style')
-    })
-    dropdownItems.forEach((e)=>{
-      e.classList.remove('show-dropdown')
-    })
+
+document.getElementById('navToggle').addEventListener('click', function() {
+  const navArea = document.getElementById('navArea');
+  navArea.classList.toggle('nav-active');
+
+  // Toggle the hamburger and close icons
+  const hamburgerIcon = document.querySelector('.hamburger');
+  const closeIcon = document.querySelector('.close');
+  if (navArea.classList.contains('nav-active')) {
+      hamburgerIcon.style.display = 'none';
+      closeIcon.style.display = 'inline';
+  } else {
+      hamburgerIcon.style.display = 'inline';
+      closeIcon.style.display = 'none';
   }
-}
+});
 
-addEventListener('resize',removeStyle)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -75,14 +59,6 @@ function addAnimation(){
 }
 
 
-// ==== PRE-LOADER FUNCTION ======
-let loader = document.getElementById("preloader")
-
-setTimeout(()=>{
-  window.addEventListener("load",function(){
-    loader.style.display = "none";
-  })
-},100)
 
 
 
